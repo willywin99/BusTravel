6f65472b070f56e49cc1616c78a70186ebabbf6c
willywin99 <willywinsianturi@gmail.com>
2021-04-29 05:07:55 +0700

    memasang template admin dashboard

---
+++ resources/views/admin/partials/footer.blade.php
@@ -0,0 +1,14 @@
+<!-- Footer -->
+<footer class="footer mt-auto">
+    <div class="copyright bg-white">
+      <p>
+        &copy; <span id="copy-year"></span> Copyright Sleek Dashboard Bootstrap Template by <a class="text-primary"
+          href="http://www.iamabdus.com/" target="_blank">Abdus</a>.
+      </p>
+    </div>
+    <script>
+      var d = new Date();
+      var year = d.getFullYear();
+      document.getElementById("copy-year").innerHTML = year;
+    </script>
+  </footer>
